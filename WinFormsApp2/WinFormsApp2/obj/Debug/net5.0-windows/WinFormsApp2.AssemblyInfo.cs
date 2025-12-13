399164741996691b024dee95f3e95a6e73d66401
Berkeulger2002 <oberul.1ger@gmail.com>
2025-12-13 15:46:30 +0300

    yeni

--- WinFormsApp2/WinFormsApp2/obj/Debug/net5.0-windows/WinFormsApp2.AssemblyInfo.cs
+++ WinFormsApp2/WinFormsApp2/obj/Debug/net5.0-windows/WinFormsApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ef1c39bb0d3f9ae56c64f3c62753724660797d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
